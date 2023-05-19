afa3b3a6c47799ea9d0f543524ae945bb3b1e767
Ivan Trj <ivantrajanovski@gmail.com>
2023-05-19 15:47:42 +0200

    style search bar

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,28 @@
-import { View, TextInput } from "react-native";
+import { View, TextInput, StyleSheet } from "react-native";
 import { FontAwesome } from "@expo/vector-icons";
+import { elevation } from "../commpn/styles";
 
 export default function Search() {
   return (
-    <View>
-      <FontAwesome name="search" />
-      <TextInput placeholder="restaurants, food" />
+    <View style={[styles.container, styles.elevation]}>
+      <FontAwesome name="search" size={25} />
+      <TextInput style={styles.input} placeholder="restaurants, food" />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: "row",
+    marginHorizontal: 25,
+    marginTop: 5,
+    backgroundColor: "white",
+    padding: 15,
+    borderRadius: 40,
+  },
+  elevation,
+  input: {
+    fontSize: 20,
+    marginLeft: 10,
+  },
+});
