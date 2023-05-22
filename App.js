44bf9352f96c3a05e5b4acab487c43af59a29848
Ivan Trajanovski <ivantrajanovski@gmail.com>
2023-05-22 15:18:42 +0200

    add props to category item

--- App.js
+++ App.js
@@ -9,7 +9,18 @@ export default function App() {
     <View style={styles.container}>
       <Header />
       <Search />
-      <CategoryItem />
+      <CategoryItem
+        name="Burger"
+        imageUrl={require("./src/assets/images/burger.png")}
+      />
+      <CategoryItem
+        name="Pizza"
+        imageUrl={require("./src/assets/images/pizza.png")}
+      />
+      <CategoryItem
+        name="Dessert"
+        imageUrl={require("./src/assets/images/cake.png")}
+      />
       <StatusBar style="auto" />
     </View>
   );
--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -1,16 +1,17 @@
 import { View, Text, StyleSheet, Image } from "react-native";
 import { elevation } from "../commpn/styles";
 
-export default function CategoryItem() {
+export default function CategoryItem({ name, imageUrl }) {
+
   return (
     <View style={[styles.container, styles.elevation]}>
       <View style={styles.imageContainer}>
         <Image
-          source={require("../assets/images/burger.png")}
+          source={imageUrl}
           style={styles.image}
         />
       </View>
-      <Text style={styles.header}>Burger</Text>
+      <Text style={styles.header}>{name}</Text>
     </View>
   );
 }
